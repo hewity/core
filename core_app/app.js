6d3d1bb0401863bfda9d5e312187274603d8b542
Joshua <Joshua.Nezianya@gmail.com>
2016-05-09 16:49:02 -0400

    resolving merge conflict in atom.js

--- core_app/app.js
+++ core_app/app.js
@@ -10,11 +10,12 @@
     RouterFunction
   ])
   .controller("PostsIndexController", PostsIndexControllerFunc)
+  .controller("PostsShowController", PostsShowControllerFunc)
   .factory("PostFactory", PostFactoryFunc);
 
   PostFactoryFunc.$inject=["$resource"];
   function PostFactoryFunc($resource){
-    return $resource("http://localhost:3000/posts/:id.json", {})
+    return $resource("http://localhost:3000/posts/:id.json", {});
   }
 
   function RouterFunction($stateProvider){
@@ -24,19 +25,39 @@
       controller: "PostsIndexController",
       controllerAs: "postsIndexVm"
     })
+    .state("postsShow", {
+      url: "/posts/:id",
+      templateUrl: "core_app/posts/show.html",
+      controller: "PostsShowController",
+      controllerAs: "postsShowVm"
+    })
   }
 
-PostsIndexControllerFunc.$inject=["$state", "PostFactory"]
+PostsIndexControllerFunc.$inject=["$state", "PostFactory"];
 function PostsIndexControllerFunc($state, PostFactory){
   var postsIndexVm = this;
-  postsIndexVm.posts = PostFactory.query()
+  postsIndexVm.posts = PostFactory.query();
   postsIndexVm.newPost= new PostFactory();
 
   postsIndexVm.create = function() {
     postsIndexVm.newPost.$save().then(function(){
       $state.go("postsIndex", {}, {reload: true});
-    })
+    });
   }
+}
+
+PostsShowControllerFunc.$inject =["PostFactory", "$stateParams"];
+function PostsShowControllerFunc(PostFactory, $stateParams) {
+  var postsShowVm = this;
+  postsShowVm.post = PostFactory.get({id: $stateParams.id});
+
+  postsShowVm.update = function(){
+  postsShowVm.post.$update({id: $stateParams.id});
+};
+
+  postsShowVm.delete = function(){
+    postsShowVm.post.$delete({id: $stateParams.id});
+  };
 }
 
 })();
